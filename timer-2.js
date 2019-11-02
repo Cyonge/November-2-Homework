1db29192d936137cd9390285a872a69d1058a195
Christian Yonge <christianyonge93@gmail.com>
2019-11-02 17:48:30 -0500

    last push for homework 4

--- timer-2.js
+++ timer-2.js
@@ -2,9 +2,12 @@ var startCountDown = document.querySelector('#startQuiz');
 var secCountDown = document.querySelector('#seconds');
 var startScreen = document.querySelector("#startScreen");
 var questionsAll = document.querySelector("#questionsAll");
-var choices = document.querySelector('#choices');
-var currentQuestion = 0;
+var currentAnswer;
 
+
+
+
+var currentQuestion = 0;
 var timeLeft = 75;
 var interval;
 var running = false;
@@ -35,25 +38,48 @@ function startTimer() {
     }, 1000);
 };
 
-console.log(startScreen.classList);
+// console.log(startScreen.classList);
 
 
 startCountDown.addEventListener('click', startQuiz);
 
-function nextQuestion() {
+function firstQuestion() {
     var title = questions[currentQuestion].title; //Use this similarly for line 60 and loop through each choice and create a new document element and appending this to the divs that are already created
     document.querySelector("#title").innerText = title;
-    currentQuestion++;
     startScreen.classList.add('hide');
     questionsAll.classList.remove('hide');
 
+    
+
+    //for loop to display the answer choices within each question in the object on the page
+    for (var i = 0; i < questions[currentQuestion].choices.length; i++) {
+
+        //this creates a button in the document
+        var buttonEl = document.createElement("button");
+
+        buttonEl.setAttribute("class", "currentAnswer");
+
+        //display the button on the page with "text.Content"
+        buttonEl.textContent = questions[currentQuestion].choices[i];
+
+        //append button to the body
+        buttonChoice.appendChild(buttonEl);
+        currentAnswer = document.getElementsByClassName("currentAnswer");
+        currentAnswer[i].addEventListener("click", questionClick);
+        
+
+        
+    }
+   
 }
+
+
+
+
 
 function startQuiz() {
     startTimer();
-    nextQuestion();
-
-
+    firstQuestion();
 
 
     //select the startScreen, as a queryselector, once you select the element and .addclass or .removeclass you can add or remove classes
@@ -64,9 +90,11 @@ function startQuiz() {
 }
 
 
-//The above code is for my timer and below I am going to be creating my 
-//object filled with my questions
 
+
+//The above code is for my timer and below I am going to be creating my 
+
+//object filled with my questions
 var questions = [
     {
         title: "What movie did Francis Ford Coppola direct?",
@@ -78,21 +106,45 @@ var questions = [
         choices: ["25%", "125%", "500%", "250%"],
         answer: "500%"
     },
+    {
+        title: "The lineup in The Usual Suspects was originally meant to be serious, but Benicio del Toro couldn’t stop _________ so they laughed throughout.",
+        choices: ["Farting", "Crying", "Cracking Jokes", "Sleeping"],
+        answer: "500%"
+    },
+
     ///etc.
 ];
 
-var mouseEventsEl = document.querySelector("#click-events");
+
+console.log(title);
 
 
-//for loop to display the answer choices within each question in the object
-for (var i = 0; i < questions[0].choices.length; i++) {
 
-    //this creates a button in the document
-    var buttonEl = document.createElement("button");
+//click and check if question is right or wrong
+//then update time and display time
+//currentQuestion++ (this increases question by 1) var currentQuestions = questions[currentQuestionIndex] - will need to declare this variable
 
-    //display the button on the page with "text.Content"
-    buttonEl.textContent = questions[0].choices[i];
+
+//This is a function that says if the quiz taker choose the right answer then they move to the next question
+//If they do not choose the right question then move to the next question and subtract 15 seconds
+function questionClick(rightAnswer) {
+    console.log("VALUE", rightAnswer.target.innerText);
+    if (rightAnswer.value === questions[currentQuestion].answer) {
+        currentQuestion++;
+        
+    }else{
+        timeLeft -= 15;
+        
+    }
+
+    firstQuestion();
     
-    //append button to the body
-    choices.appendChild(buttonEl);
-}
+};
+
+
+
+// buttonChoice.addEventListener("click", questionClick());
+
+
+// $("#currentAnswer").click(questionClick());
+
